4691fc86713a1979434c7da5abd92f1a4f14478c
Adam Sir <Adam.sir@servian.com>
2020-02-07 10:19:42 +1100

    Added in new files for extra features

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -1,10 +1,13 @@
 resource "aws_subnet" "subnetmodule" {
-#   vpc_id     = aws_vpc.main.id
   vpc_id = var.vpc_id
   cidr_block = var.cidr
   availability_zone = var.availability_zone
-
   tags = {
     Name = var.subnet_name
   }
+}
+
+output "subnet_output" {
+  value       = aws_subnet.subnetmodule.id
+  description = "Subnet"
 }
---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "subnet_output" {
+  value       = module.private_subnet_az1.subnet_output
+  description = "Subnet"
+}
